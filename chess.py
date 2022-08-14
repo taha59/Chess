8b8f6291725cd4185f0b0aa3a4e613f9003311c6
taha59 <tahatablet16@gmail.com>
2022-08-13 20:10:02 -0400

    empty window added

--- chess.py
+++ chess.py
@@ -1,5 +1,8 @@
 import pygame as p
-from Chess import ChessEngine
+import pygame
+import turtle
+
+FPS = 60
 
 def main():
 
@@ -20,10 +23,28 @@ def main():
     max_fps = 15
     images = {}
 
+    window = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("CHESS")
+
     #add the unique chess pieces to chess_pieces variable
     chess_pieces = list(set(board[0])) + list(set(board[1])) + list(set(board[6])) + list(set(board[7]))
-    for i in chess_pieces:
-        print(i)
+
+    # pygame.init()
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+    
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
+    
